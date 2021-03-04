16273a499c6dd00fa769a87f6ba624fd43713e6a
cjsmith1988 <75030108+cjsmith1988@users.noreply.github.com>
2021-03-03 19:43:10 -0500

    Update database.js

--- db/database.js
+++ db/database.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Ranfunk1988*',
+    password: '',
     database: 'employeeManagementDB'
   });
   
